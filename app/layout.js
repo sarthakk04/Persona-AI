47a7bb493c9de9c3d93f085c33ab326f25ee2aec
Sarthak Shinde <sartech2005@gmail.com>
2025-10-14 20:12:28 +0530

    Favicon Updated

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,14 @@
 import "./globals.css";
 import ClientWrapper from "./clientWrapper";
 import { Analytics } from "@vercel/analytics/next";
+// import { icons } from "lucide-react";
 
 export const metadata = {
   title: "Persona AI",
   description: "Talk with any persona you can imagine.",
+  icons: {
+    icon: "/favi.png",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -13,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Favicon */}
-        <link rel="icon" href="/logo.png" />
+        {/* <link rel="icon" href="/logo.png" /> */}
       </head>
       <body>
         <Analytics />
